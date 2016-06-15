228d2a40bbda5f9fd0de0e165a0bd9b5e51c6b04
Wang Xiaolin <wx672ster@gmail.com>
2016-06-15 22:38:30 +0800

    initial

---
+++ doc/latex/swfu/swfcthesis/sample/hello.c
@@ -0,0 +1,37 @@
+#include "打狗棒法.h"
+
+int main(int argc, char *argv[])
+{
+  while detect("恶狗来袭") {
+      if ("正面来袭")
+	{
+	  fight("使一招“恶狗拦路”");
+	}
+      else if ("斜刺里杀来")
+	{
+	  fight("使一招“斜打狗背”");
+	}
+      else if ("打狗棒被抢")
+	{
+	  fight("使一招“獒口夺杖”");
+	}
+      else if ("背地里扑来")
+	{
+	  fight("使一招“反戳狗臀”");
+	}
+      else if ("四下里杀来")
+	{
+	  fight("使一招“天下无狗”");
+	}
+      else if ("千万条恶狗杀来")
+	{
+	  run("as fast as possible.\n");
+	}
+      else if ("苦无脱身之计")
+	{
+	  cry("loudly.\n");
+	}
+      else pray("sincerely until die.\n");
+    }
+  return 0;
+}
